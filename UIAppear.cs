6c64bd983f7fd9b0401dc2f45083da28993defcc
Abby Noble <tech.substance@gmail.com>
2020-07-09 20:42:15 -0400

    Add files via upload
    My UI Script to make a canvas appear upon entering a trigger collider.

---
+++ UIAppear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField] private Canvas customeImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+      if(other.CompareTag("Player"))
+      {
+        customeImage.enabled = true;
+      }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+      if(other.CompareTag("Player"))
+      {
+        customeImage.enabled = false;
+      }
+    }
+
+
+}
