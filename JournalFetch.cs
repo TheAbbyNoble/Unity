96e0e01e9d3203a845dbb678498c45f799658a9a
Abby Noble <tech.substance@gmail.com>
2020-12-16 09:30:39 -0500

    Add files via upload
    Allows you to pause the game to look at a journal canvas by pressing J.

---
+++ JournalFetch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalFetch : MonoBehaviour
+{
+
+  public GameObject journalUI;
+  public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.J))
+      {
+        if(GameIsPaused)
+        {
+          Resume();
+        }
+          else
+        {
+          Pause();
+        }
+      }
+    }
+
+    public void Resume()
+    {
+      journalUI.SetActive(false);
+      Time.timeScale = 1f;
+      GameIsPaused = false;
+      Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+      journalUI.SetActive(true);
+      Time.timeScale = 0f;
+      GameIsPaused = true;
+      Cursor.lockState = CursorLockMode.None;
+    }
+
+
+
+
+
+}
